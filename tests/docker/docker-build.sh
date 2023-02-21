e029da888aae7da9ca2b33d819a9e36c2890c550
Elias Häußler <e.haeussler@familie-redlich.de>
2023-02-21 08:40:35 +0100

    [TASK] Add cache layer to Docker test suite

--- tests/docker/docker-build.sh
+++ tests/docker/docker-build.sh
@@ -6,24 +6,38 @@ readonly root_path="$(cd -- "$(dirname "$0")/../.." >/dev/null 2>&1; pwd -P)"
 readonly image_name="project-builder-test"
 readonly expected="This command cannot be run in non-interactive mode."
 
-# Check verbose mode
-if [ "-v" = "$1" ] || [ "--verbose" = "$1" ]; then
-    readonly verbose=1
-fi
+# Resolve parameters
+cache=0
+verbose=0
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case ${key} in
+    -c | --cache)
+        cache=1
+        shift
+        ;;
+    -v | --verbose)
+        verbose=1
+        shift
+        ;;
+    esac
+done
 
 # Build and tag new image
-printf "Building image... "
-docker build -q -t "$image_name" "$root_path" >/dev/null
+if [ "$cache" -eq 1 ]; then
+    printf "Building image from cache... "
+    docker build --cache-from "$image_name" -q -t "$image_name" "$root_path" >/dev/null
+else
+    printf "Building image... "
+    docker build -q -t "$image_name" "$root_path" >/dev/null
+fi
 printf "\033[0;32mDone\033[0m\n"
 
 # Run image
 printf "Running image... "
-set +e; readonly output="$(docker run --rm "$image_name" 2>&1)"; set -e
-printf "\033[0;32mDone\033[0m\n"
-
-# Remove image
-printf "Removing image... "
-docker rmi "$image_name" >/dev/null
+set +e
+readonly output="$(docker run --rm "$image_name" 2>&1)"
+set -e
 printf "\033[0;32mDone\033[0m\n"
 
 # Print output
@@ -35,7 +49,7 @@ fi
 
 # Check output
 if [[ $output != *"$expected"* ]]; then
-    >&2 echo -e "\n🚨 Failed."
+    echo >&2 -e "\n🚨 Failed."
     exit 1
 fi
 
