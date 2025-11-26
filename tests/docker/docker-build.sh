e57d41c16d56010e781eaa2db5130044d2f7d011
Elias Häußler <e.haeussler@familie-redlich.de>
2025-11-26 10:29:36 +0100

    [!!!][TASK] Warn if /app directory is not mounted in Docker container

--- tests/docker/docker-build.sh
+++ tests/docker/docker-build.sh
@@ -2,9 +2,11 @@
 # shellcheck disable=SC2155
 set -e
 
+GREEN="\033[0;32m"
+NC="\033[0m"
+
 readonly root_path="$(cd -- "$(dirname "$0")/../.." >/dev/null 2>&1; pwd -P)"
 readonly image_name="project-builder-test"
-readonly expected="This command cannot be run in non-interactive mode."
 
 # Resolve parameters
 cache=0
@@ -41,14 +43,16 @@ else
     printf "Building image... "
     docker build -q -t "$image_name" --build-arg "PROJECT_BUILDER_VERSION=$root_version" "$root_path" >/dev/null
 fi
-printf "\033[0;32mDone\033[0m\n"
+printf "${GREEN}Done${NC}\n"
 
 # Run image
-printf "Running image... "
+printf "Running image (with mounted volume)... "
 set +e
-readonly output="$(docker run --rm "$image_name" 2>&1)"
+tmpDir="$(mktemp)"
+rm "$tmpDir"
+output="$(docker run --rm -v "$tmpDir":/app "$image_name" 2>&1)"
 set -e
-printf "\033[0;32mDone\033[0m\n"
+printf "${GREEN}Done${NC}\n"
 
 # Print output
 if [ "$verbose" = 1 ]; then
@@ -58,6 +62,28 @@ if [ "$verbose" = 1 ]; then
 fi
 
 # Check output
+expected="This command cannot be run in non-interactive mode."
+if [[ $output != *"$expected"* ]]; then
+    echo >&2 -e "\n🚨 Failed."
+    exit 1
+fi
+
+# Run image
+printf "Running image (without mounted volume)... "
+set +e
+output="$(docker run --rm "$image_name" 2>&1)"
+set -e
+printf "${GREEN}Done${NC}\n"
+
+# Print output
+if [ "$verbose" = 1 ]; then
+    echo -e "\n============================================================\n"
+    echo "$output"
+    echo -e "\n============================================================"
+fi
+
+# Check output
+expected="It seems like the /app directory is not mounted to your host device."
 if [[ $output != *"$expected"* ]]; then
     echo >&2 -e "\n🚨 Failed."
     exit 1
